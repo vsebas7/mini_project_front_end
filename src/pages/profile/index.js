839d41e1e57d7f5acccb585b9db4d08fcb079668
Vincent Sebastian <vincent.sbst@gmail.com>
2023-06-15 17:19:43 +0700

    update

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,23 +1,25 @@
 import {React,useState,useRef, } from "react";
 import {useDropzone} from 'react-dropzone';
-import { Navigate } from "react-router-dom"
+import { Navigate,useNavigate } from "react-router-dom"
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { editProfileSchema } from "../../store/slices/auth/validation";
 import { useDispatch, useSelector } from "react-redux";
 import { changeEmail, changePhone, changeUsername, uploadProfilePic } from "../../store/slices/auth/slices";
 import bgimage from '../../assets/image.svg'
+import Navbar from "../../components/navbar";
 
 import "../../Form.scss"
 
 const ProfileUser = () => {
   const dispatch = useDispatch() 
-  const {id,username, email, phone,imgProfile} = useSelector(state=>{
+  const navigate = useNavigate()
+  const {username, email, phone,imgProfile,isLogin} = useSelector(state=>{
     return {
-      id : state.auth.id,
       username : state.auth.username,
       phone : state.auth.phone,
       email : state.auth.email,
       imgProfile : state.auth.imgProfile,
+      isLogin : state.auth.isLogin
     }
   })
   
@@ -53,7 +55,7 @@ const ProfileUser = () => {
   const emailRef = useRef()
   const phoneRef = useRef()
 
-  if (!id) {
+  if (!isLogin) {
     return <Navigate to="/login" replace/>
   }
     
@@ -80,7 +82,7 @@ const ProfileUser = () => {
       dispatch(uploadProfilePic(formData))
     }
     onButtonCancelUpload()
-    window.my_modal_1.close()
+    window.modalConfirmation.close()
   }
     
     return (
@@ -94,138 +96,171 @@ const ProfileUser = () => {
     >
       {({ errors, touched,}) => {
         return (
-          <div className="container">
-            <div className="form card w-96 bg-base-100 shadow-xl">
-              <Form>
-              <h1>User Details</h1>
-              <div className="avatar flex flex-row justify-center align-middle">
-                <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img src={process.env.REACT_APP_IMAGE_URL + imgProfile} />
-                </div>
-              </div>
-              {/* <img src={process.env.REACT_APP_IMAGE_URL + imgProfile}></img> */}
-              <br/>
-                <div className="form-row">
-                  {/* <label>Profile Picture</label> */}
-                  <div className="flex flex-row gap-5">
-                    <button className="btn flex-grow-0" onClick={()=>window.my_modal_5.showModal()}>{file?.name ? file.name : "Upload Profile Picture"} </button> 
-                    <button className={`btn btn-square btn-outline flex-none ${file?.name ? "" : "hidden"}`} onClick={onButtonCancelUpload}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                    </button> 
+          <div className="w-full h-full px-40 py-10">
+            <Navbar/>
+            <div className="container">
+              <div className="form card w-4/12 bg-base-100 shadow-xl">
+                <Form >
+                <h1>User Details</h1>
+                <div className="avatar flex flex-row justify-center align-middle mt-7">
+                  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src={process.env.REACT_APP_IMAGE_URL + imgProfile} />
                   </div>
-                  <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                    <form method="dialog" className="modal-box">
-                      <h3 className="font-bold text-lg">Upload your image</h3>
-                      <p className="py-4">File should be jpg, jpeg, webp or png and less than 1Mb</p>
-                      <div className={`alert alert-error ${file?.code ? "" : "hidden"}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                        <span>{file?.code}</span>
-                      </div>
-                      {file?.name 
-                        ? 
-                          <div className="stats shadow">
-                            <div className="stat">
-                              <div className="stat-title">{file.name}</div>
-                            </div>                            
-                          </div> 
-                        : 
-                          <div className='flex flex-col h-auto drop-shadow-lg p-5 justify-between bg-white w-full rounded-md'>
-                            <div  {...getRootProps({className :`h-auto] w-full bg-light-grey ${isDragActive ? 'border-amber-950':'border-light-blue'} border-2 border-dashed rounded-md`})}>
-                              <input {...getInputProps({name : 'image'})}/>
-                              <img src={bgimage} className='max-w-1/3 mx-auto mt-4' />
-                              <p className='text-slate-400 md:text-md text-md text-center mt-2 mb-4 '>Drag & Drop your image here</p>
-                            </div>
-                            <div className="flex flex-col">
-                              <p className='text-center font-normal text-slate-400 text-md mt-2 mb-2'>Or</p>
-                              <a onClick={open} className='link link-hover bg-blue text-amber-950 font-normal p-1 rounded-lg w-auto mx-auto px-4 py-2 text-md '>Choose a file</a>
-                            </div>
-                          </div>
-                      }
-                      <div className="modal-action">
-                        <button className="btn" onClick={onButtonCancelUpload}>Cancel</button>
-                        <button className="btn btn-accent" onClick={onButtonSave}>Save</button>
-                      </div>
-                    </form>
-                  </dialog>
-                  <br/>
-                  <label htmlFor="username">Username</label>
-                  <Field
-                    type="text"
-                    name="username"
-                    id="username"
-                    defaultValue = {username}
-                    placeholder = {username}
-                    innerRef = {usernameRef}
-                    className={
-                      errors.username && touched.username ? "input-error input input-md w-full max-w-xs" : "input input-bordered input-md w-full max-w-xs"
-                    }
-                  />
-                  <ErrorMessage name="username" component="span" className="error" />
                 </div>
-
-                <div className="form-row">
-                  <label htmlFor="email">Email</label>
-                  <Field
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder = {email}
-                    innerRef = {emailRef}
-                    className={
-                      errors.email && touched.email ? "input-error input input-md w-full max-w-xs" : "input input-bordered input-md w-full max-w-xs"
-                    }
-                  />
-                  <ErrorMessage name="email" component="span" className="error" />
-                </div>
-                <div className="form-row">
-                  <label htmlFor="phone">Phone</label>
-                  <Field
-                    type="phone"
-                    name="phone"
-                    id="phone"
-                    defaultValue = {phone}
-                    placeholder = {phone}
-                    innerRef = {phoneRef}
-                    className={
-                      errors.phone && touched.phone ? "input-error input input-md w-full max-w-xs" : "input input-bordered input-md w-full max-w-xs"
-                    }
-                  />
-                  <ErrorMessage
-                    name="phone"
-                    component="span"
-                    className="error"
-                  />
-                </div>
-
-                <button 
-                  className={`btn btn-neutral`} 
-                  onClick={()=>window.my_modal_1.showModal()}
-                >
-                  Save Changes
-                </button>
-                <dialog id="my_modal_1" className="modal">
-                  <form method="dialog" className="modal-box">
-                    <h3 className="font-bold text-lg">Confirmation!</h3>
-                    <p className="py-4">Are you sure wanna apply this change(s)</p>
-                    <div className="modal-action">
-                      <button className="btn">Close</button>
+                <br/>
+                  <div className="form-row">
+                    <div className="flex flex-row gap-5 justify-center align-middle">
+                      <button 
+                        className="btn justify-center" 
+                        onClick={()=>window.modalUploadImageProfile.showModal()}>
+                          {file?.name ? file.name : "Upload Profile Picture"} 
+                      </button> 
                       <button
-                        type="button"
-                        className="btn btn-accent"
-                        onClick={onButtonSaveProfile}
-                      >
-                        Apply
-                      </button>
+                        className={`btn btn-square btn-outline flex-none ${file?.name ? "" : "hidden"}`} 
+                        onClick={onButtonCancelUpload}
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                      </button> 
                     </div>
-                  </form>
-                </dialog>
+                    <dialog id="modalUploadImageProfile" className="modal modal-bottom sm:modal-middle">
+                      <Form method="dialog" className="modal-box">
+                        <h3 className="font-bold text-lg">Upload your image</h3>
+                        <p className="py-4">File should be jpg, jpeg, webp or png and less than 1Mb</p>
+                        <div className={`alert alert-error ${file?.code ? "" : "hidden"}`}>
+                          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                          <span>{file?.code}</span>
+                        </div>
+                        {file?.name 
+                          ? 
+                          <div className="stats shadow">
+                              <div className="stat">
+                                <div className="stat-title">{file.name}</div>
+                              </div>                            
+                            </div> 
+                          : 
+                          <div className='flex flex-col h-auto drop-shadow-lg p-5 justify-between bg-white w-full rounded-md'>
+                              <div  {...getRootProps({className :`h-auto] w-full bg-light-grey ${isDragActive ? 'border-amber-950':'border-light-blue'} border-2 border-dashed rounded-md`})}>
+                                <input {...getInputProps({name : 'image'})}/>
+                                <img src={bgimage} className='max-w-1/3 mx-auto mt-4' />
+                                <p 
+                                  className='text-slate-400 md:text-md text-md text-center mt-2 mb-4 '
+                                >
+                                  Drag & Drop your image here
+                                </p>
+                              </div>
+                              <div className="flex flex-col">
+                                <p className='text-center font-normal text-slate-400 text-md mt-2 mb-2'>
+                                  Or
+                                </p>
+                                <a 
+                                  onClick={open} 
+                                  className='link link-hover bg-blue text-amber-950 font-normal p-1 rounded-lg w-auto mx-auto px-4 py-2 text-md '
+                                >
+                                  Choose a file
+                                </a>
+                              </div>
+                            </div>
+                        }
+                        <div className="modal-action">
+                          <button 
+                            className="btn" 
+                            onClick={onButtonCancelUpload}
+                          >
+                            Cancel
+                          </button>
+                          <button className="btn btn-accent" onClick={onButtonSave}>Save</button>
+                        </div>
+                      </Form>
+                    </dialog>
+                    <br/>
+                    <label htmlFor="username">Username</label>
+                    <Field
+                      type="text"
+                      name="username"
+                      id="username"
+                      defaultValue = {username}
+                      placeholder = {username}
+                      innerRef = {usernameRef}
+                      className={
+                        errors.username && touched.username ? "input-error input input-md w-full " : "input input-bordered input-md w-full "
+                      }
+                    />
+                    <ErrorMessage name="username" component="span" className="error" />
+                  </div>
 
-                <a href='/change-password' >Change Password?</a>
-                
-              </Form>
-              <br/>
-              <a href='#' >My Favorite Blog</a>
-              <a href='#' >My Published Blog</a>
+                  <div className="form-row">
+                    <label htmlFor="email">Email</label>
+                    <Field
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder = {email}
+                      innerRef = {emailRef}
+                      className={
+                        errors.email && touched.email ? "input-error input input-md w-full " : "input input-bordered input-md w-full "
+                      }
+                    />
+                    <ErrorMessage 
+                      name="email" 
+                      component="span" 
+                      className="error" 
+                    />
+                  </div>
+                  <div className="form-row">
+                    <label htmlFor="phone">Phone</label>
+                    <Field
+                      type="phone"
+                      name="phone"
+                      id="phone"
+                      defaultValue = {phone}
+                      placeholder = {phone}
+                      innerRef = {phoneRef}
+                      className={
+                        errors.phone && touched.phone ? "input-error input input-md w-full " : "input input-bordered input-md w-full "
+                      }
+                      />
+                    <ErrorMessage
+                      name="phone"
+                      component="span"
+                      className="error"
+                    />
+                  </div>
+                  <div className="flex justify-between">
+                    <button 
+                      className={`btn btn-neutral`} 
+                      onClick={()=>window.modalConfirmation.showModal()}
+                    >
+                      Save Changes
+                    </button>
+                    <dialog id="modalConfirmation" className="modal">
+                      <Form method="dialog" className="modal-box">
+                        <h3 className="font-bold text-lg">Confirmation!</h3>
+                        <p className="py-4">Are you sure wanna apply this change(s)</p>
+                        <div className="modal-action">
+                          <button className="btn">Close</button>
+                          <button
+                            type="button"
+                            className="btn btn-accent"
+                            onClick={onButtonSaveProfile}
+                          >
+                            Apply
+                          </button>
+                        </div>
+                      </Form>
+                    </dialog>
+                    
+                    <button 
+                      className="btn btn-ghost"
+                      onClick={()=>navigate("/change-password")}
+                    >
+                      Change Password?
+                    </button>
+                  </div>
+                  
+                </Form>
+                <br/>
+                <a href='#' >My Favorite Blog</a>
+              </div>
             </div>
           </div>
         );
